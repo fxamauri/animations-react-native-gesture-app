82767ae1285eba8250f8957b0331c1cd24d93ac9
amauri dias <athlon.amauri@gmail.com>
2021-11-24 13:40:41 -0300

    ajuste escala original ao soltar carta no pan gesture

--- src/Card.tsx
+++ src/Card.tsx
@@ -8,10 +8,12 @@ import Animated, {
     Easing,
     useAnimatedGestureHandler,
     useAnimatedStyle,
-    useSharedValue, withSpring, withTiming
+    useSharedValue,
+    withSpring,
+    withTiming,
+    withDelay
 } from 'react-native-reanimated';
 import {snapPoint} from "react-native-redash";
-import {scryRenderedDOMComponentsWithClass} from "react-dom/test-utils";
 
 interface CardProps {
     card: {
@@ -19,9 +21,10 @@ interface CardProps {
     };
 }
 
-const { width: wWidth } = Dimensions.get("window");
+const { width: wWidth, height } = Dimensions.get("window");
 
 const aspectRatio = 722 / 368;
+const DURATION = 250;
 const CARD_WIDTH = wWidth - 128;
 const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
 const IMAGE_WIDTH = CARD_WIDTH * 0.9;
@@ -29,31 +32,42 @@ const side = (wWidth + CARD_WIDTH + 50) / 2;
 const SNAP_POINTS = [-side, 0, side];
 
 
-export const Card = ({ card: { source } }: CardProps) => {
+export const Card = ({ card: { source }, index }: CardProps) => {
     const x = useSharedValue(0);
-    const y = useSharedValue(0);
+    const y = useSharedValue(-height);
     const rotateZ = useSharedValue(Math.random() * 20 - 10);
     const scale = useSharedValue(1);
 
+    React.useEffect(() => {
+        const delay = index * DURATION;
+        y.value = withDelay(
+            delay,
+            withTiming(0,{
+                duration: DURATION,
+                easing: Easing.inOut(Easing.ease),
+            })
+        )
+    }, []);
     const onGestureEvent = useAnimatedGestureHandler<
         PanGestureHandlerGestureEvent,
         { x: number, y: number }
         >({
-            onStart: (_, ctx) => {
-                ctx.x = x.value;
-                ctx.y = y.value;
-                scale.value = withTiming(1.1, { easing: Easing.inOut(Easing.ease) });
-                rotateZ.value = withTiming(0, { easing: Easing.inOut(Easing.ease) })
-            },
-            onActive: ({ translationX, translationY }, ctx) => {
-                x.value = ctx.x + translationX;
-                y.value = ctx.y + translationY;
-            },
-            onEnd: ({ velocityX, velocityY }) => {
-                const dest = snapPoint(x.value, velocityX, SNAP_POINTS);
-                x.value = withSpring(dest, { velocity: velocityX })
-                y.value = withSpring(0, { velocity: velocityY })
-            }
+        onStart: (_, ctx) => {
+            ctx.x = x.value;
+            ctx.y = y.value;
+            scale.value = withTiming(1.1, { easing: Easing.inOut(Easing.ease) });
+            rotateZ.value = withTiming(0, { easing: Easing.inOut(Easing.ease) })
+        },
+        onActive: ({ translationX, translationY }, ctx) => {
+            x.value = ctx.x + translationX;
+            y.value = ctx.y + translationY;
+        },
+        onEnd: ({ velocityX, velocityY }) => {
+            const dest = snapPoint(x.value, velocityX, SNAP_POINTS);
+            x.value = withSpring(dest, { velocity: velocityX })
+            y.value = withSpring(0, { velocity: velocityY });
+            scale.value = withTiming(1, { easing: Easing.inOut(Easing.ease) })
+        }
     });
 
     const style = useAnimatedStyle(() => ({
